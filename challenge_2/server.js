0ccc4b50b285a73156bb12127cda7e10eb9b5133
owegami <busimeiru@gmail.com>
2020-09-18 16:45:09 -0700

    "Add features to html file and body parser to server"

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+
+app.use(bodyParser.json);
+
 app.use(express.static(__dirname + '/client'));
+
 
 app.post('/', (req, res) => {
   res.send('Got a POST request at port 3000');
