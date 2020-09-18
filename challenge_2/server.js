8ddab81d014c4f36ca8e0bda9b5e3ac2613f87c7
owegami <busimeiru@gmail.com>
2020-09-18 16:27:16 -0700

    "Begin building a server and have the html page served via express server"

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static(__dirname + '/client'));
 
+app.post('/', (req, res) => {
+  res.send('Got a POST request at port 3000');
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+})
