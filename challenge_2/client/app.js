9a38c4298e3447c465d5a54a8555f81d827e3293
owegami <busimeiru@gmail.com>
2020-09-19 10:57:15 -0700

    "Fix package.json to meet requirements and add functionality to post request"

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,12 @@
+const jsonFetcher = () => {
+  let data = $('#JSONtext').val();
+  console.log(data);
 
+  // fetch('/JSONData', {
+  //   method: 'POST',
+  //   mode: 'same-origin',
+  //   cache: 'no-cache'
+  //   credentials:
 
+  // })
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,17 +1,53 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 
-app.use(bodyParser.json);
-
+app.use(express.json());
 app.use(express.static(__dirname + '/client'));
 
-
-app.post('/', (req, res) => {
-  res.send('Got a POST request at port 3000');
+app.get('/JSONData', (req, res) => {
+  res.send('Got a GET request at port 8000');
 })
+
+app.post('/JSONData', (req, res) => {
+  let data = req.body;
+  let keys = Object.keys(data);
+  keys.pop();
+  console.log(keys);
+  let props = [];
+  let result = [];
+
+  const recurseData = (node) => {
+    // debugger;
+    // console.log('node:', node);
+    for (let i = 0; i < node.length; i++) {
+      let tempArray = [];
+      for (let key in node[i]) {
+        if (key != 'children') {
+          tempArray.push(node[i][key]);
+        }
+      }
+      // debugger;
+      // console.log('temp:', tempArray);
+      props.push(tempArray);
+      if (node[i].children.length > 0) {
+        // debugger;
+        recurseData(node[i].children);
+      }
+    }
+  }
+
+  recurseData(data.children);
+  // console.log('props:', props);
+  result.push(keys.join());
+  for (let i = 0; i < props.length; i++) {
+    result.push(props[i].join());
+  }
+
+  res.send(result.join('\n'));
+})
+
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
